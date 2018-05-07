0efb3ce4b18ced9ad2bb006305f9186b645d347f
nzri <nykolaz01@gmail.com>
2018-05-06 20:40:45 -0700

    Update yes.js

--- multitable/yes.js
+++ multitable/yes.js
@@ -1,21 +1,11 @@
-var table = 5;                                       // Unit of table
-var operator = 'multiplication';                     // Type of calculation
-var i = 1;                                           // Set counter to 1
-var msg = '<h2>Multiplication Table</h2>';           // Message
+var table = prompt("Enter number for the table...");
+var i = 1;
+var msg = '<h2>Multiplication Table</h2>';
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
+while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
