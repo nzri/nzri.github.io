6d39dfe72fcb1e55e2e5b44dc16533dbb67b3cb0
nzri <nykolaz01@gmail.com>
2018-06-07 17:58:54 -0700

    Update and rename slideshow/slideshow.js to prog109/slideshow/slideshow.js

--- prog109/slideshow/slideshow.js
+++ prog109/slideshow/slideshow.js
@@ -1,4 +1,4 @@
-var myImages =["/slideshow/rainbow.jpg","/slideshow/blackyellow.jpg","/slideshow/wild.jpg","/slideshow/beaut.jpg","/slideshow/falcon.JPG"];
+var myImages =["/prog109/slideshow/rainbow.jpg","/prog109/slideshow/blackyellow.jpg","/prog109/slideshow/wild.jpg","/prog109/slideshow/beaut.jpg","/prog109/slideshow/falcon.JPG"];
 
 
 var captionImages =["Rainbow Bird","Black and Yellow Bird","Wild Bird","Beautiful Bird","Falcon"];
